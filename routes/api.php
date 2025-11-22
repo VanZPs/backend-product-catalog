c8db28f631c31cb171e47e64b96def5f73e58b8b
MohamadSolkhanNawawi <mohamadsolkhan558@gmail.com>
2025-11-22 15:15:03 +0700

    feat(location): integrate laravolt indonesia and add location endpoints

--- routes/api.php
+++ routes/api.php
@@ -7,3 +7,9 @@ Route::post('/register', [AuthController::class, 'register']);
 Route::post('/login',    [AuthController::class, 'login']);
 Route::post('/logout',   [AuthController::class, 'logout']);
 Route::get('/me',        [AuthController::class, 'me']);
+
+Route::get('/locations/provinces', [LocationController::class, 'provinces']);
+Route::get('/locations/cities/{province}', [LocationController::class, 'cities']);
+Route::get('/locations/districts/{city}', [LocationController::class, 'districts']);
+Route::get('/locations/villages/{district}', [LocationController::class, 'villages']);
+
